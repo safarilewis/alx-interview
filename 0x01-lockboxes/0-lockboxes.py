5759292c332200a7eeb803477d9b409ec415ae15
safarilewis <lleewwii1@gmail.com>
2023-04-20 08:33:08 +0300

    First Upload

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from typing import List
+"""
+You are given a list of lists named boxes.
+Each box is numbered sequentially from 0 to n - 1
+
+In each list in box is a given set of numbers, on of the numbers might be the key to the next box.
+A key with the same number as a box opens that box
+"""
+
+
+def canUnlockAll(boxes: List[List]) -> bool:
+    pass
---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Checks if a number is a valid utf8 encoding'''
+from typing import List
+
+
+def validUTF8(data: List[int]):
+    '''Checks numbers in data and returns True if all are valid utf8'''
+    count = 0
+    for num in data:
+        if count == 0:
+            if (num >> 5) == 0b110:
+                count = 1
+            elif (num >> 4) == 0b1110:
+                count = 2
+            elif (num >> 3) == 0b11110:
+                count = 3
+            elif (num >> 7) != 0:
+                return False
+        else:
+            if (num >> 6) != 0b10:
+                return False
+            count -= 1
+    return count == 0
