2a5e1dadc91baba95c85e8c79090c4041ae7f2eb
safarilewis <lleewwii1@gmail.com>
2023-05-06 22:31:42 +0300

    semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,26 +3,26 @@ const request = require('request');
 const URI = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
 
-request(URI, function(error,_,body){
-    if(!error) {
-        const charactersURI = JSON.parse(body).characters;
-        getCharacters(charactersURI, 0)
-    }
-    else {
-        console.error(error)
-    }
+request(URI, function (error, _, body) {
+  if (!error) {
+    const charactersURI = JSON.parse(body).characters;
+    getCharacters(charactersURI, 0)
+  }
+  else {
+    console.error(error)
+  }
 });
 
-function getCharacters(character, id) {
-    request(character[id], function(error,_,body) {
-        if (!error) {
-            console.log(JSON.parse(body).name);
-            if (id + 1 < character.length) {
-                getCharacters(character, id + 1);
-            }
-        else {
-            console.error(error);
-            }
-         }
-    })
+function getCharacters (character, id) {
+  request(character[id], function (error, _, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (id + 1 < character.length) {
+        getCharacters(character, id + 1);
+      }
+      else {
+        console.error(error);
+      }
+    }
+  })
 }
