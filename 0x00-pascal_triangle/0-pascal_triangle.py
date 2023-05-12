759714d1281b7daa8e1babd53f53354dd7a0bda4
safarilewis <lleewwii1@gmail.com>
2023-05-13 01:22:27 +0300

    removing imports

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """Pascal's Triangle"""
-from math import factorial
 
 
 def pascal_triangle(n):
     '''Returns a list of ints representing pascal's triangle'''
-
-    if n <= 0:
-        return []
-    triangle = []
-    for i in range(n):
+    for i in range(0, n + 1):
         row = []
-        for j in range(i+1):
-            row.append(factorial(i)//(factorial(j)*factorial(i-j)))
-        print(row)
-        row = []
+        tri = []
+        new_row = []
+        new_row = [j > 0 and j < i - 1 and i > 2 and row[j-1] +
+                   row[j] or 1 for j in range(0, i)]
+        row = new_row
+        tri += [new_row]
+    return tri[]
