b7ecbf76f2248fd4a5a9734710be1f6f8eb1f047
safarilewis <lleewwii1@gmail.com>
2023-05-13 01:12:59 +0300

    pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Pascal's Triangle"""
+from math import factorial
+
+
+def pascal_triangle(n):
+    '''Returns a list of ints representing pascal's triangle'''
+
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i+1):
+            row.append(factorial(i)//(factorial(j)*factorial(i-j)))
+        print(row)
+        row = []
