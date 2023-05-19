03dee866c28b1a39150b4040f584f64401dbc808
safarilewis <lleewwii1@gmail.com>
2023-05-19 18:09:27 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,4 +9,14 @@ A key with the same number as a box opens that box
 
 
 def canUnlockAll(boxes) -> bool:
-    pass
+    '''Function that checks if all boxes can be opened'''
+    check = 1
+    for box in boxes:
+        for item in box:
+            if item == check:
+                value = True
+            else:
+                value = False
+                break
+        check = check + 1
+    return value
