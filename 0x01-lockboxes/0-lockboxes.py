9c622e2698744464e19af0aa3e002a8bc03ad72b
safarilewis <lleewwii1@gmail.com>
2023-05-19 18:21:39 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,7 +3,8 @@
 You are given a list of lists named boxes.
 Each box is numbered sequentially from 0 to n - 1
 
-In each list in box is a given set of numbers, on of the numbers might be the key to the next box.
+In each list in box is a given set of numbers,
+one of the numbers might be the key to the next box.
 A key with the same number as a box opens that box
 """
 
@@ -11,12 +12,15 @@ A key with the same number as a box opens that box
 def canUnlockAll(boxes) -> bool:
     '''Function that checks if all boxes can be opened'''
     check = 1
+    position = 1
+    keys = []
     for box in boxes:
         for item in box:
-            if item == check:
+            keys.append(item)
+    while position < len(boxes):
+        for key in keys:
+            if key == position:
                 value = True
-            else:
-                value = False
-                break
-        check = check + 1
+                position = position + 1
+            else: value = False
     return value
