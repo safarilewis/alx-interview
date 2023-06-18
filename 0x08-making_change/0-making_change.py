6ab0545832c064654b42a6746d546244bbc6ae23
safarilewis <lleewwii1@gmail.com>
2023-06-18 23:28:21 +0300

    efficiency test 2

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,17 +4,18 @@
 
 def makeChange(coins, total):
     """Function that makes change from given coins"""
-    # if total is 0 or total < 0:
-    #     return 0
-    # if coins == [] or coins is None:
-    #     return -1
+    if total is 0 or total < 0:
+        return 0
+    if coins == [] or coins is None:
+        return -1
+    remainder = total
     coins_count = 0
     coin_id = 0
     sorted_coins = sorted(coins, reverse=True)
     n = len(coins)
-    while rem > 0:
-        if rem - sorted_coins[coin_id] >= 0:
-            rem -= sorted_coins[coin_id]
+    while remainder > 0:
+        if remainder - sorted_coins[coin_id] >= 0:
+            remainder -= sorted_coins[coin_id]
             coins_count += 1
         else:
             coin_id += 1
