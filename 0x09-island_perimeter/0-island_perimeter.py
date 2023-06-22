d0189900d8c5f900faec3eaedf8ba327fea260e9
safarilewis <lleewwii1@gmail.com>
2023-06-22 23:05:04 +0300

    fixing bug in rows loop

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,7 +11,7 @@ def island_perimeter(grid):
     for r in range(rows):
         for c in range(columns):
             if grid[r][c] == 1:
-                if r == 0 or grid[i - 1][c] == 0:
+                if r == 0 or grid[r - 1][c] == 0:
                     perimeter += 1
                 if r == rows - 1 or grid[r + 1][c] == 0:
                     perimeter += 1
